1c79f5cbc7373463aa0e63e016152fc03e0e0f0a
Donal-Breen-05 <donalbreen50@gmail.com>
2025-10-30 22:58:51 +0000

    uploaded main.py
    majority of the code is written 
    
    need to fix 
    - formating of buttons 
    - style of app 
    - adding a drop down menu 
    - custom icon

---
+++ main.py
@@ -0,0 +1,69 @@
+from tkinter import * 
+import tkinter as tk
+import tkinter.messagebox as mb 
+import tkinter.filedialog as filedialog
+import os , sys 
+
+#name = 
+
+
+""" 
+    Functions 
+""" 
+def fileButton(): 
+    print("Button clicked")
+
+def saveFile(): 
+    
+    # find the path to save file 
+    path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")])
+    
+    # saves all of the text area to the file 
+    with open(path, "w" ) as file: 
+        file.write(textArea.get(1.0, tk.END))
+    
+
+def openFile():
+    path = filedialog.askopenfilename(title="Select a Text File", filetypes=[("Text files", "*.txt"),  ("All Files", "*.*")])
+    if path:
+        with open(path, 'r') as file:
+            content = file.read()
+            textArea.delete(1.0, tk.END)  # Clear previous content
+            textArea.insert(tk.END, content)
+
+
+
+"""
+    GUI loop
+""" 
+# Create the main window
+root = tk.Tk()
+root.title("text editor") 
+
+#open file button 
+openButton = tk.Button(root, text="Open", command=openFile)
+openButton.pack()
+
+
+#save button 
+saveButton = tk.Button(root, text="Save", command=saveFile)
+saveButton.pack()
+
+# Set the window size
+root.geometry("700x600")
+root.resizable(0, 0)
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+
+#scrollbar 
+scrollbar = Scrollbar(root)
+scrollbar.pack(side=RIGHT,fill=Y) 
+
+textArea = Text(root,
+                 yscrollcommand=scrollbar.set)
+textArea.pack(fill=BOTH)
+
+# Run the application
+root.mainloop()
+
