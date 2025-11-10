1d29a74f3b7b401d75dcf6947db64f778d5b6360
Donal Breen <donalbreen50@gmail.com>
2025-11-10 14:37:46 +0000

    general fixes
    - fixed dark mode to change the colour of backgroud and buttons

--- main.py
+++ main.py
@@ -43,14 +43,38 @@ def openFile():
 def colourMode(): 
  #   if 
     global mode 
+    global bg_colour
     if mode == "light": 
         mode = "dark"
+        #change bg_colour 
+        bg_colour = "#2d2d2d"
+        root.configure(bg=bg_colour)
+
+        #change buttons 
         textArea.config(bg="#242528", fg="white", insertbackground="white")
         colourButton.config(text="dark")
+        openButton.config(bg="#242528", fg="white")
+        closeButton.config(bg="#242528", fg="white")
+        saveButton.config(bg="#242528", fg="white")
+        colourButton.config(bg="#242528", fg="white")
+        buttonFrame.config(bg="#2d2d2d")
+
     else:           
         mode = "light"
+        #change bg_colour 
+        bg_colour = "white"
+
+        #change buttons 
         textArea.config(bg="white", fg="black", insertbackground="black")
         colourButton.config(text="light")
+        openButton.config(bg="white", fg="black")
+        closeButton.config(bg="white", fg="black")
+        saveButton.config(bg="white", fg="black")
+        colourButton.config(bg="white", fg="black")
+        buttonFrame.config(bg="white")
+        root.configure(bg=bg_colour)
+
+        
 
 
 
@@ -59,6 +83,7 @@ def colourMode():
 
 #initialise the colout mode to be light mode 
 mode = "light" 
+bg_colour ="white"
 """
     GUI loop
 """ 
@@ -72,24 +97,27 @@ root.resizable(0, 0)
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
+#set window colour 
+root.configure(bg=bg_colour)
+
 #button frame 
-buttonFrame = tk.Frame(root, height=50)
+buttonFrame = tk.Frame(root, height=50, bg="white")
 buttonFrame.pack(side=TOP, fill=X, pady=5)
 
 #open file button 
-openButton = tk.Button(buttonFrame,text="Open", command=openFile)
+openButton = tk.Button(buttonFrame,text="Open", command=openFile, bg="white", fg="black")
 openButton.pack( side=LEFT, padx=10)
 
 #save button 
-saveButton = tk.Button(buttonFrame, text="Save", command=saveFile)
+saveButton = tk.Button(buttonFrame, text="Save", command=saveFile , bg="white" , fg="black")
 saveButton.pack(side =LEFT , padx=10)
 
 #closeFile 
-closeButton =tk.Button(buttonFrame, text="Close" , command=closeFile) 
+closeButton =tk.Button(buttonFrame, text="Close" , command=closeFile, bg="white", fg="black") 
 closeButton.pack(side=LEFT , padx=10)
 
 #colour button 
-colourButton = tk.Button(buttonFrame, text=mode , command=colourMode)
+colourButton = tk.Button(buttonFrame, text=mode , command=colourMode, bg="white" , fg="black")
 colourButton.pack(side=LEFT, padx=10)
 
 #scrollbar 
